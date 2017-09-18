7d0942ece3ee23c67e7539e6a1747830112b26f6
bodang2016 <bodang@qq.com>
2017-09-18 17:13:48 +0100

    first update

---
+++ src/mycode/Day3.java
@@ -0,0 +1,34 @@
+package mycode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day3 {
+
+	public int lengthOfLongestSubstring(String s) {
+		int ret = 0;
+		List<Character> attempt = new ArrayList<Character>();
+		char[] splited = s.toCharArray();
+		for (int i = 0; i < splited.length; i++) {
+			for (int j = i; j < splited.length; j++) {
+				if (!attempt.contains(splited[j])) {
+					attempt.add(splited[j]);
+					ret = ret > attempt.size() ? ret : attempt.size();
+				} else {
+					break;
+				}
+			}
+			attempt.clear();
+			if (splited.length - i < ret) {
+				break;
+			}
+		}
+		return ret;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
